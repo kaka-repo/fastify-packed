a8c1f5cf388cffae577a4b6879f3b101385b8380
KaKa <kaka@kakawebsitedemo.com>
2022-04-28 16:09:56 +0800

    feat: plugin migration

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,12 @@
+import type { default as FastifyCORS, FastifyCorsOptions } from '@fastify/cors'
+import type { default as FastifyHelmet } from '@fastify/helmet'
+import type { default as FastifyJWT } from '@fastify/jwt'
+import type { default as FastifyMongoDB } from '@fastify/mongodb'
+import type { default as FastifSwagger } from '@fastify/swagger'
 import * as crypto from 'crypto'
 import type { default as EnvSchema, EnvSchemaData, EnvSchemaOpt } from 'env-schema'
 import { FastifyPluginAsync, FastifyPluginCallback, FastifyRegisterOptions } from 'fastify'
-import type { default as FastifyCORS, FastifyCorsOptions } from 'fastify-cors'
-import type { default as FastifyHelmet } from 'fastify-helmet'
-import type { default as FastifyJWT } from 'fastify-jwt'
-import type { default as FastifyMongoDB } from 'fastify-mongodb'
 import FastifyPlugin from 'fastify-plugin'
-import type { default as FastifSwagger } from 'fastify-swagger'
 import * as fs from 'fs'
 import * as path from 'path'
 import type { default as UnderPressure } from 'under-pressure'
@@ -38,7 +38,7 @@ function parseOption<T> (option: boolean | T, def: T): T {
 
 type ExtractGeneric<T> = T extends FastifyPluginCallback<infer X> ? X : T extends FastifyPluginAsync<infer Y> ? Y : never
 
-export interface FastifyPackedOption extends Object {
+export interface FastifyPackedOption {
   env?: boolean | EnvSchemaOpt
   cors?: boolean | FastifyRegisterOptions<FastifyCorsOptions>
   helmet?: boolean | FastifyRegisterOptions<ExtractGeneric<typeof FastifyHelmet>>
@@ -64,21 +64,28 @@ declare module 'fastify' {
   }
 }
 
-export function createPackedPlugin (envOptions?: true | EnvSchemaOpt): {
+export interface CreatedPackedPlugin {
   plugin: FastifyPluginAsync<FastifyPackedOption>
   version: () => string
   env: EnvSchemaData
-} {
+}
+
+export interface CreatePackedOption {
+  overrideVersion?: string
+}
+
+export function createPackedPlugin (envOptions?: true | EnvSchemaOpt, options?: CreatePackedOption): CreatedPackedPlugin {
   // environment variables
   let env: EnvSchemaData = {}
-  const envSchema = requireOptionalDependency<typeof EnvSchema>('env-schema')
-  if (envSchema !== false && checkEnable(envOptions)) {
+  if (checkEnable(envOptions)) {
+    const envSchema = requireOptionalDependency<typeof EnvSchema>('env-schema')
+    if (envSchema === false) throw Error('please install env-schema with the following command:\nnpm install env-schema\nyarn add env-schema')
     const opt = parseOption(envOptions, { })
     env = envSchema(opt)
   }
 
   return {
-    plugin: FastifyPlugin(Packed, {
+    plugin: FastifyPlugin(createPacked(options), {
       fastify: '3.x',
       name: 'fastify-packed',
       dependencies: []
@@ -88,67 +95,88 @@ export function createPackedPlugin (envOptions?: true | EnvSchemaOpt): {
   }
 }
 
-const Packed: FastifyPluginAsync<FastifyPackedOption> = async function (fastify, options) {
-  options = options ?? {}
+const createPacked = function (options?: CreatePackedOption): FastifyPluginAsync<FastifyPackedOption> {
+  return async function Packed (fastify, options) {
+    options = options ?? {}
 
-  // environment variables
-  const envSchema = requireOptionalDependency<typeof EnvSchema>('env-schema')
-  if (envSchema !== false && checkEnable(options.env)) {
-    const opt = parseOption(options.env, { })
-    const env = envSchema(opt)
-    await fastify.decorate('config', env)
-    await fastify.decorate('VERSION', version())
+    // environment variables
+    if (checkEnable(options.env)) {
+      const envSchema = requireOptionalDependency<typeof EnvSchema>('env-schema')
+      if (envSchema === false) throw Error('please install env-schema with the following command:\nnpm install env-schema\nyarn add env-schema')
+      const opt = parseOption(options.env, { })
+      const env = envSchema(opt)
+      await fastify.decorate('config', env)
+      await fastify.decorate('VERSION', version())
+    }
+
+    // cross-origin resource sharing
+    if (checkEnable(options.cors)) {
+      const fastifyCORS = requireOptionalDependency<typeof FastifyCORS>('@fastify/cors')
+      if (fastifyCORS === false) throw Error('please install @fastify/cors with the following command:\nnpm install @fastify/cors\nyarn add @fastify/cors')
+      overrideVersion(fastifyCORS)
+      const opt = parseOption(options.cors, { origin: true })
+      await fastify.register(fastifyCORS, opt)
+    }
+
+    // swagger should place before helmet
+    if (checkEnable(options.swagger)) {
+      const fastifySwagger = requireOptionalDependency<typeof FastifSwagger>('@fastify/swagger')
+      if (fastifySwagger === false) throw Error('please install @fastify/swagger with the following command:\nnpm install @fastify/swagger\nyarn add @fastify/swagger')
+      overrideVersion(fastifySwagger)
+      const opt = parseOption(options.swagger, { })
+      await fastify.register(fastifySwagger, opt)
+    }
+
+    // security package
+    if (checkEnable(options.helmet)) {
+      const fastifyHelmet = requireOptionalDependency<typeof FastifyHelmet>('@fastify/helmet')
+      if (fastifyHelmet === false) throw Error('please install @fastify/helmet with the following command:\nnpm install @fastify/helmet\nyarn add @fastify/helmet')
+      overrideVersion(fastifyHelmet)
+      const opt = parseOption(options.helmet, { contentSecurityPolicy: fastifyHelmet.contentSecurityPolicy.getDefaultDirectives() })
+      await fastify.register(fastifyHelmet, opt)
+    }
+
+    // rate limit
+    if (checkEnable(options.underPressure)) {
+      const underPressure = requireOptionalDependency<typeof UnderPressure>('under-pressure')
+      if (underPressure === false) throw Error('please install under-pressure with the following command:\nnpm install under-pressure\nyarn add under-pressure')
+      overrideVersion(underPressure)
+      const opt = parseOption(options.underPressure, {
+        maxEventLoopDelay: 1000,
+        message: 'System is under pressure, please try again later.',
+        retryAfter: 50,
+        exposeStatusRoute: true
+      })
+      await fastify.register(underPressure, opt)
+    }
+
+    // jwt instance
+    if (checkEnable(options.jwt)) {
+      const fastifyJWT = requireOptionalDependency<typeof FastifyJWT>('@fastify/jwt')
+      if (fastifyJWT === false) throw Error('please install @fastify/jwt with the following command:\nnpm install @fastify/jwt\nyarn add @fastify/jwt')
+      overrideVersion(fastifyJWT)
+      const opt = parseOption(options.jwt, { secret: crypto.randomBytes(8).toString('hex') })
+      await fastify.register(fastifyJWT, opt)
+    }
+
+    // mongodb instance
+    if (checkEnable(options.mongodb)) {
+      const fastifyMongoDB = requireOptionalDependency<typeof FastifyMongoDB>('@fastify/mongodb')
+      if (fastifyMongoDB === false) throw Error('please install @fastify/mongodb with the following command:\nnpm install @fastify/mongodb\nyarn add @fastify/mongodb')
+      overrideVersion(fastifyMongoDB)
+      const opt = parseOption(options.mongodb, { url: 'mongodb://127.0.0.1:27017', database: 'default' })
+      await fastify.register(fastifyMongoDB, opt)
+    }
   }
 
-  // cross-origin resource sharing
-  const fastifyCORS = requireOptionalDependency<typeof FastifyCORS>('fastify-cors')
-  if (fastifyCORS !== false && checkEnable(options.cors)) {
-    const opt = parseOption(options.cors, { origin: true })
-    await fastify.register(fastifyCORS, opt)
-  }
-
-  // swagger should place before helmet
-  const fastifySwagger = requireOptionalDependency<typeof FastifyCORS>('fastify-swagger')
-  if (fastifySwagger !== false && checkEnable(options.swagger)) {
-    const opt = parseOption(options.swagger, { })
-    await fastify.register(fastifySwagger, opt)
-  }
-
-  // security package
-  const fastifyHelmet = requireOptionalDependency<typeof FastifyHelmet>('fastify-helmet')
-  if (fastifyHelmet !== false && checkEnable(options.helmet)) {
-    const opt = parseOption(options.helmet, { contentSecurityPolicy: fastifyHelmet.contentSecurityPolicy.getDefaultDirectives() })
-    await fastify.register(fastifyHelmet, opt)
-  }
-
-  // rate limit
-  const underPressure = requireOptionalDependency<typeof UnderPressure>('under-pressure')
-  if (underPressure !== false && checkEnable(options.underPressure)) {
-    const opt = parseOption(options.underPressure, {
-      maxEventLoopDelay: 1000,
-      message: 'System is under pressure, please try again later.',
-      retryAfter: 50,
-      exposeStatusRoute: true
-    })
-    await fastify.register(underPressure, opt)
-  }
-
-  // jwt instance
-  const fastifyJWT = requireOptionalDependency<typeof FastifyJWT>('fastify-jwt')
-  if (fastifyJWT !== false && checkEnable(options.jwt)) {
-    const opt = parseOption(options.jwt, { secret: crypto.randomBytes(8).toString('hex') })
-    await fastify.register(fastifyJWT, opt)
-  }
-
-  // mongodb instance
-  const fastifyMongoDB = requireOptionalDependency<typeof FastifyMongoDB>('fastify-mongodb')
-  if (fastifyMongoDB !== false && checkEnable(options.mongodb)) {
-    const opt = parseOption(options.mongodb, { url: 'mongodb://127.0.0.1:27017', database: 'default' })
-    await fastify.register(fastifyMongoDB, opt)
+  function overrideVersion (plugin: any): void {
+    if (typeof options?.overrideVersion === 'string') {
+      plugin[Symbol.for('')] = options.overrideVersion
+    }
   }
 }
 
-export const FastifyPacked = FastifyPlugin(Packed, {
+export const FastifyPacked = FastifyPlugin(createPacked(), {
   fastify: '3.x',
   name: '@kakang/fastify-packed',
   dependencies: []
